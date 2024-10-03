110bd5155cd148c0f4ef9c3a3bc22c0db3b4aebd
Chi Leung Fung <ChiLeung.Fung@aia.co.uk>
2024-10-03 12:44:54 +0100

    Added an end point to reset accounts.

--- src/routes/bank_accounts.ts
+++ src/routes/bank_accounts.ts
@@ -47,6 +47,65 @@ router.get('/', (req: Request, res: Response) => {
     res.json(bankAccounts);
 });
 
+// Remove all existing bank accounts and populate with demo data
+router.get('/reset-accounts', (req: Request, res: Response) => {
+    // Remove all existing account data
+    bankAccounts.splice(0);
+
+    // Populate bank accounts list with demo data
+    bankAccounts.push({
+        id: bankAccounts.length + 1,
+        account_name: "Travel funds",
+        description: "Funds for future travels and events.",
+        balance: 500.00,
+        owner: 1,
+    });
+    bankAccounts.push({
+        id: bankAccounts.length + 1,
+        account_name: "Car collection",
+        description: "Money pot for car collection.",
+        balance: 8750.00,
+        owner: 1,
+    });
+    bankAccounts.push({
+        id: bankAccounts.length + 1,
+        account_name: "Savings account",
+        description: "Funds for cool events.",
+        balance: 110.00,
+        owner: 2,
+    });
+    bankAccounts.push({
+        id: bankAccounts.length + 1,
+        account_name: "Book collection",
+        description: "Library collection.",
+        balance: 231.26,
+        owner: 3,
+    });
+    bankAccounts.push({
+        id: bankAccounts.length + 1,
+        account_name: "Account no. 98",
+        description: "Duplicate account - keep maxing bank liability limit.",
+        balance: 10000.00,
+        owner: 4,
+    });
+    bankAccounts.push({
+        id: bankAccounts.length + 1,
+        account_name: "Account no. 99",
+        description: "Duplicate account - keep maxing bank liability limit.",
+        balance: 9876.66,
+        owner: 4,
+    });
+    bankAccounts.push({
+        id: bankAccounts.length + 1,
+        account_name: "Travel funds",
+        description: "Funds for future travels and events.",
+        balance: 768.35,
+        owner: 5,
+    });
+
+    res.json(bankAccounts);
+});
+
 // Get a single account
 router.get('/:id', (req: Request, res: Response) => {
     const account = bankAccounts.find((u) => u.id === parseInt(req.params.id));
