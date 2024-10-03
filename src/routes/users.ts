9f9956237603466eaedc6ebd701e83a868c2f77a
Chi Leung Fung <ChiLeung.Fung@aia.co.uk>
2024-10-03 10:33:32 +0100

    Added a reset User endpoint to make the demo easier to work with.

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -37,6 +37,56 @@ router.get('/', (req: Request, res: Response) => {
     res.json(usersList);
 });
 
+// Remove existing User list and populate with demo data
+router.get('/reset-users', (req: Request, res: Response) => {
+    // Remove all previous user data - bank accounts will also need to be removed
+    usersList.splice(0);
+
+    // Populate with demo data
+    usersList.push({
+        id: usersList.length + 1,
+        user_name: 'first_User',
+        first_name: 'Scott',
+        family_name: 'Summers',
+        is_active: true,
+    });
+
+    usersList.push({
+        id: usersList.length + 1,
+        user_name: 'second_User',
+        first_name: 'Robert',
+        family_name: 'Drake',
+        is_active: true,
+    });
+
+    usersList.push({
+        id: usersList.length + 1,
+        user_name: 'third_User',
+        first_name: 'Henry',
+        family_name: 'McCoy',
+        is_active: true,
+    });
+
+    usersList.push({
+        id: usersList.length + 1,
+        user_name: 'fourth_User',
+        first_name: 'Warren',
+        family_name: 'Worthington',
+        is_active: true,
+    });
+
+    usersList.push({
+        id: usersList.length + 1,
+        user_name: 'fifth_User',
+        first_name: 'Jean',
+        family_name: 'Grey',
+        is_active: true,
+    });
+
+    // Return list of demo users
+    res.json(usersList);
+});
+
 // Get a single user
 router.get('/:id', (req: Request, res: Response) => {
     const user = usersList.find((t) => t.id === parseInt(req.params.id));
